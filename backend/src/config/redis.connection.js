7d27743ac4cf88e424bdeff3435bb80cc44ee5cc
Yash Pal <yashpal45678@gmail.com>
2026-01-17 22:45:35 +0530

    Concuurently running

--- backend/src/config/redis.connection.js
+++ backend/src/config/redis.connection.js
@@ -1,6 +1,9 @@
 import IORedis from 'ioredis';
+import dotenv from 'dotenv';
 
-const redisConnection=new IORedis('redis://default:uFoHAQnQAtsNgg0w53tDA4DEXQvUs8K0@redis-14164.crce217.ap-south-1-1.ec2.cloud.redislabs.com:14164',
+dotenv.config();
+
+const redisConnection=new IORedis(process.env.REDIS_URL,
     {
         maxRetriesPerRequest: null
     }
